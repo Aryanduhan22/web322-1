fb0cd09fe4344f1d295eb899fc9824248afb6aaf
Aryan Duhan <105321528+itsAryanDuhan@users.noreply.github.com>
2023-04-10 00:25:56 -0400

    vguy

--- auth-service.js
+++ auth-service.js
@@ -17,7 +17,7 @@ let User;
 
 module.exports.initialize = function () {
     return new Promise(function (resolve, reject) {
-        let db = mongoose.createConnection("mongodb+srv://itsaryanduhan:aryan1044@senecaweb.olctilq.mongodb.net/?retryWrites=true&w=majority");
+        let db = mongoose.createConnection("mongodb+srv://mhakimi7:amYdV6SleLf4iPZv@senecaweb.6ao4hoy.mongodb.net/?retryWrites=true&w=majority");
 
         db.on('error', (err)=>{
             reject(err); // reject the promise with the provided error
